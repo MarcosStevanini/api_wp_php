a8395d159837628dcef01cad1c5a51e0614b3bb6
MarcosStevanini <marcosstevanini@gmail.com>
2020-08-31 09:14:20 -0300

    Consumindo api em WP/PHP

---
+++ consumindo-api.php
@@ -0,0 +1,61 @@
+<?php 
+function consumindoApi()
+{
+	$request = wp_remote_post('https://api.cartaoparceiro.com.br/graphql', [
+		'headers' => [
+			'Content-Type' => 'application/json',
+		],
+		'body' => wp_json_encode([
+			'query' => '
+						{
+							getAccrediteds{
+								id
+								firstName
+								lastName    
+								accredited{
+									phone,
+									address{
+										city
+										street
+										number
+										state
+										neighborhood
+									},
+									
+									category{
+										title
+									},
+									
+									subCategories{
+										title
+									}
+								}    
+							}
+						}
+				'
+		])
+	]);
+	$decoded_response = json_decode($request['body'], true);
+
+
+	foreach ($decoded_response['data']['getAccrediteds'] as $credenciados) {
+		$accredited = $credenciados['accredited'];
+		$category = $accredited['category']['title'];
+		$address = $accredited['address'];
+
+		foreach ($accredited['subCategories'] as $subCategories) {
+			$subCategory = $subCategories['title'];
+		}
+
+		echo " 
+			<tr>
+				<td>$category</td>
+				<td>$subCategory</td>
+				<td>$credenciados[firstName] $credenciados[lastName]</td>
+				<td>$address[street] , $address[number], $address[neighborhood]</td>
+				<td>$address[city]/$address[state]</td>
+				<td class='listTel'>$accredited[phone]</td>
+			</tr>";
+	}
+}
+?>
